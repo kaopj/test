fe8477994af932d2557bf9358ea42b7d1e337b16
kaopj <166913067+kaopj@users.noreply.github.com>
2024-07-04 18:03:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ y = radius * np.sin(theta)
 z = radius*radius - x*x - y*y;
 a = st.sidebar.button('Menu')
 b = st.sidebar.button('Nagorn loves ut toot')
+c = st.sidebar.write('test')
 df = pd.DataFrame({
     "x": x,
     "y": y,
