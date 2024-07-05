f65833e343c3a428b5d8a0e572264d773a078301
kaopj <166913067+kaopj@users.noreply.github.com>
2024-07-05 22:08:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,42 +7,32 @@ import streamlit as st
 # Welcome to NagornisGay!
 # x!
 """
-
+def click():
+    st.balloons()
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 size = st.slider("size", 0, 10)
 indices = np.linspace(0, size, num_points)
 theta = 2 * np.pi * num_turns * indices
 radius = np.linspace(0, size, num_points)
-num_gay = st.button("press"," ")
-"""# whuzza """+str(num_gay)
+chat = st.chat_input()
+st.button("press",on_click=click)
+with st.sidebar.form(key='my_form'):
+    username = st.text_input('Username')
+    password = st.text_input('Password')
+    st.form_submit_button('Login')
+st.download_button('truth','nagorn is gay','the truth')
 x = radius * np.cos(theta)
 y = radius * np.sin(theta)
-z = radius*radius - x*x - y*y;
+z = radius*radius - x*x - y*y
 a = st.sidebar.button('Menu')
 b = st.sidebar.button('Nagorn loves ut toot')
-st.echo()
-with st.echo():
-    """
-    #include <iostream>
-    #include <vector>
-    using namespace std;
-    #define int long long
-    #define be(x) x.begin(),x.end()
-    #define lgm cin.tie(0)->sync_with_stdio(0);
-    int main() {
-        lgm;
-        int n;
-        cin >> n;
-    }
-    """
 df = pd.DataFrame({
     "x": x,
     "y": y,
     "idx": indices,
     "rand": np.random.randn(num_points),
 })
-
 st.altair_chart(alt.Chart(df, height=700, width=700)
     .mark_point(filled=True)
     .encode(
